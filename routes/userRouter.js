6df32d5a453ca44d74d0e1d2950add17fefffe42
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-11-24 15:51:26 +0600

    restricted users api to admin only

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -14,7 +14,7 @@ const router = express.Router();
 // routes
 router.post("/", createUser);
 router.get("/:email", getUser);
-router.get("/", getAllUsers);
+router.get("/", verifyId, verifyAdmin, getAllUsers);
 router.put("/:email", updateUser);
 router.delete("/:email", verifyId, verifyAdmin, deleteUser);
 
