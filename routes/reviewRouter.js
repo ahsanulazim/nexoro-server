e583b8acae24553334956bcd8ce42ec2937f33d1
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-12-09 01:38:02 +0600

    second server branch

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createReview } from "../controllers/reviewController.js";
+import upload from "../middleware/uploadCloudinary.js";
 
 const router = express.Router();
 
 //Routes
-router.post("/", createReview);
+router.post("/", upload.none(), createReview);
 
 export default router;
