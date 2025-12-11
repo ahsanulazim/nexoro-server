36504b8d3dd527cfab85309fcf09012fd8a4524b
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-12-11 23:07:29 +0600

    service api working 2

--- routes/teamRouter.js
+++ routes/teamRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { addMember, deleteMember, getAllMembers, updateMember } from "../controllers/teamController.js";
 import upload from "../middleware/uploadCloudinary.js";
+import { deleteFromCloudinary } from "../middleware/deleteCloudinary.js";
 
 const router = express.Router();
 
 //Routes
 router.post("/", upload.single("profilePic"), addMember);
 router.get("/", getAllMembers);
-router.delete("/:id", deleteMember);
+router.delete("/:id", deleteFromCloudinary, deleteMember);
 router.put("/:id", upload.single("profilePic"), updateMember);
 
 export default router;
