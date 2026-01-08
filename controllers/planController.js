a044b7ebe6cd642541b82d6a2dcccc3d2ed1cae9
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2026-01-08 16:07:24 +0600

    plan get api

--- controllers/planController.js
+++ controllers/planController.js
@@ -19,3 +19,40 @@ export const createPlan = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const editPlan = async (req, res) => {
+  const { planName, price, benefits } = req.body;
+  const { slug, planId } = req.params;
+  try {
+    const result = await serviceCollection.updateOne(
+      { slug, "plans.id": new ObjectId(planId) },
+      {
+        $set: {
+          "plans.$.planName": planName,
+          "plans.$.price": price,
+          "plans.$.benefits": benefits,
+          "plans.$.added": new Date(),
+        },
+      }
+    );
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ message: "Service or Plan not found" });
+    }
+    res.status(200).json({ message: "Plan updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getPlans = async (req, res) => {
+  try {
+    const slug = req.params.selectedSlug;
+    const service = await serviceCollection.findOne({ slug });
+    if (!service) {
+      return res.status(404).json({ message: "Service not found" });
+    }
+    res.status(200).json(service.plans || []);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- routes/planRouter.js
+++ routes/planRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createPlan } from "../controllers/planController.js";
+import { createPlan, editPlan, getPlans } from "../controllers/planController.js";
 
 const router = express.Router();
 
 //Routes
 router.put("/:slug", createPlan);
+router.put("/:slug/:planId", editPlan);
+router.get("/:selectedSlug", getPlans);
 
 export default router;
